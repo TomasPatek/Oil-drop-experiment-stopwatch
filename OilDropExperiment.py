bf4cfffc5b139a33fc2c39c2545e8047139bbfab
Tomáš Pátek <Tomas.Patek@bednar.com>
2023-11-05 15:46:22 +0100

    Comments added

--- OilDropExperiment.py
+++ OilDropExperiment.py
@@ -3,27 +3,27 @@ import keyboard # for keylogs
 class ODEStopwatch:               
     def keyUp(self, event):        
         if self.down_start != 0:
-            print("Chyba měření")
+            print("Chyba měření")   # measuring error
             self.up_start = 0
             self.down_start = 0
         elif self.up_start == 0:
             self.up_start = event.time
-            print("Start nahoru")
+            print("Start nahoru")   # movement up start
         else:
-            print("Čas nahoru:")
+            print("Čas nahoru:")    # movement up time
             print(event.time - self.up_start)
             self.up_start = 0
         
     def keyDown(self, event):
         if self.up_start != 0:
-            print("Chyba měření")
+            print("Chyba měření")   # measuring error
             self.up_start = 0
             self.down_start = 0
         elif self.down_start == 0:
-            print("Start dolů")
+            print("Start dolů")     # movement down start
             self.down_start = event.time
         else:
-            print("Čas dolu:")
+            print("Čas dolu:")      # movement down time
             print(event.time - self.down_start)
             self.down_start = 0
             
@@ -34,7 +34,7 @@ class ODEStopwatch:
         keyboard.on_press_key(key="up", callback=self.keyUp)
         keyboard.on_press_key(key="down", callback=self.keyDown)
         
-        print("Program ukončíte stiskem tlačítka Esc")
+        print("Program ukončíte stiskem tlačítka Esc")  # Escape program by pressing Esc
         keyboard.wait("esc")       
 
 
